f07bef01204dae308d847c8ae6dcf4e881621aaa
arvindsangwan <rvndsngwn@gmail.com>
2024-05-26 00:40:21 +0530

    feat: Add barcode scanning functionality to home page
    This commit adds barcode scanning functionality to the home page of the app. It replaces the previous overlay page with a new home page that includes a button to scan barcodes. When the button is pressed, the user is taken to a barcode scanner screen where they can scan barcodes. The scanned barcode is then displayed on the home page.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:example/pages/overlay.dart';
+import 'dart:developer';
+
+import 'package:ai_barcode_scanner/ai_barcode_scanner.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +14,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: OverlayPage(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String barcode = 'Tap  to scan';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(' Scanner'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              child: const Text('Scan Barcode'),
+              onPressed: () async {
+                await Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => AiBarcodeScanner(
+                      onDispose: () {
+                        debugPrint("Barcode scanner disposed!");
+                      },
+                      controller: MobileScannerController(
+                        detectionSpeed: DetectionSpeed.noDuplicates,
+                      ),
+                      onDetect: (p0) => setState(() {
+                        barcode = p0.barcodes.first.rawValue.toString();
+                        log(barcode, name: 'Barcode');
+                      }),
+                      validator: (p0) =>
+                          p0.barcodes.first.rawValue?.startsWith('https://') ??
+                          false,
+                    ),
+                  ),
+                );
+              },
+            ),
+            Text(barcode),
+          ],
+        ),
+      ),
     );
   }
 }
